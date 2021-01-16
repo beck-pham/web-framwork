fbd0be33b16da2731b95e3d068501474653b9d55
beck_pham <chauvupham@gmail.com>
2021-01-15 16:12:36 -0800

    start implementing method and event listening system

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,5 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'becl', age: 20});
+const user = new User( { id: 1 });
 
-user.on('change', () => {
-  console.log('change #1')
-});
-
-user.on('save', () => {
-  console.log('Save was triggered')
-});
-
-user.trigger('hghjg');
+user.fetch();
